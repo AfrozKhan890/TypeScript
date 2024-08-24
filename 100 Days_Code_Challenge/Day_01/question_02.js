c5ceb16f34b8d9785142778335004f5841e02197
Afroz Khan <Afrozaftab11@gmail.com>
2024-08-24 07:28:47 +0500

    Day 01

---
+++ 100 Days_Code_Challenge/Day_01/question_02.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question 02: Personal Message: Store a person’s name in a variable and print a message to them, 
+// like “Hello Admin, would you like to learn some TypeScript today?”
+let Name = "Afroz Khan";
+console.log(`Hello ${Name}, would you like to learn some TypeScript today?`);
---
+++ 100 Days_Code_Challenge/Day_01/question_02.ts
@@ -0,0 +1,6 @@
+// Question 02: Personal Message: Store a person’s name in a variable and print a message to them, 
+// like “Hello Admin, would you like to learn some TypeScript today?”
+
+
+let Name:string = "Afroz Khan"
+console.log(`Hello ${Name}, would you like to learn some TypeScript today?`);
