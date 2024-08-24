eff3f7a81affd807ab70a0cd48bc6c8ff09534c2
Afroz Khan <Afrozaftab11@gmail.com>
2024-08-24 07:30:03 +0500

    Day 03

---
+++ 100 Days_Code_Challenge/Day_03/question_07.ts
@@ -0,0 +1,14 @@
+// Question 07: Number Eight: Write addition, subtraction, multiplication, and division operations that each result
+// in the number 8.Enclose your operations in print statements.
+
+console.log(7+1); // Addition
+console.log(20 - 12); // Subtraction
+console.log(4 * 2); // Multiplication
+console.log(20 / 2.5); // Division
+
+
+
+
+// This is not part of the question 
+// This part for extra practice
+console.log((50 + 10) / (7 * 6) + 10 - 3.428571428571429); // Complex Question
---
+++ 100 Days_Code_Challenge/Day_03/question_09.js
@@ -0,0 +1,5 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. Then, create a message revealing your 
+// favorite number. Print that message.
+let favorite_number = 6;
+console.log(`My Favorite Number is ${favorite_number}`);
