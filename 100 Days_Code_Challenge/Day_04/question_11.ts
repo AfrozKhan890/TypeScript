0aee5dbdd1eddbe18a289c873f59ba3df041544a
Afroz Khan <Afrozaftab11@gmail.com>
2024-08-25 06:49:43 +0500

    Day 04,Challenge has been completed

---
+++ 100 Days_Code_Challenge/Day_04/question_11.ts
@@ -0,0 +1,8 @@
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each
+//  person’s name by accessing each element in the list, one at a time.
+
+let friends:string[] = ["Aftab","Abid", "Murad","Jawad","Shahzaib"] // Array Declaration
+// For Loop
+for(let i = 0; i<friends.length; i++){
+    console.log(friends[i]); // Print
+}
