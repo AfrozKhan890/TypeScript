ce98834fd5a4e34eb72ee79e643bf31a58689ac6
Afroz Khan <Afrozaftab11@gmail.com>
2024-08-28 06:05:27 +0500

    Day 6 Challenge has been completed

---
+++ 100 Days_Code_Challenge/Day_06/question_16.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question 16: More Guests: You've found a bigger dinner table, so there's room for more guests.
+// When you have more space, you can add more guests to your list. You can add guests at the 
+// beginning, middle, and end of an array.
+let Guests = ["Ali Abid", "Murad Ali", "Aftab Khan"];
+console.log("\nGreat News! We have found more space for dinner table.\n");
+Guests.unshift("Shahzaib");
+Guests.push("Shayan");
+Guests.splice(4, 0, "Wahab");
+Guests.forEach(Guest => {
+    console.log(`Dear ${Guest},You have invited for dinner.`);
+});
